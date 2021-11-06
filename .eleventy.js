80677e648fa72f2fa9c0a37f9f1d2af73a0fdd09
Christine Golden <christinegolden@Dora.local>
2021-11-06 07:50:38 -0700

    Added Details page

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/assets");
-  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/js");
 
   eleventyConfig.setBrowserSyncConfig({
     files: "./public/css/**/*.css",
